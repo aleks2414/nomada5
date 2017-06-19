af72876176cbf6c9e54e024a6503a870d8e54b92
aleks2414 <alejandroromo14@gmail.com>
2017-06-18 19:25:16 -0500

    new

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-  	@trips = Trip.all.order("created_at desc").limit(6)
+  	@trips = Trip.all.order("created_at desc").limit(4)
   	@experiences_count = Experience.count
   	@trips_count = Trip.count
   end
